c5e1432c00ace56ab7f25daf5fc4bbdbd944936f
sungMinPark7733 <park64906@gmail.com>
2023-11-28 16:20:16 +1300

    first commit

---
+++ intro/src/App.tsx
@@ -0,0 +1,11 @@
+import Introduction from "./components/Introduction";
+
+function App() {
+  return (
+    <div>
+      <Introduction></Introduction>
+    </div>
+  );
+}
+
+export default App;
---
+++ intro/src/components/Introduction.tsx
@@ -0,0 +1,22 @@
+import "./Introduction.css";
+
+const Introduction = () => {
+  return (
+    <div className="container">
+      <div className="left">
+        <h1 className="heading">About me</h1>
+        <p className="intro">
+          Hi, my name is Sung Min and I am an aspiring developer with a passion
+          for problem-solving and a solid foundation in computer science. I am
+          currently pursuing a Bachelor's degree in Computer and Information
+          Sciences at Auckland University of Technology and I am dedicated to
+          expanding my knowledge and honing my skills in the dynamic field of
+          technology.
+        </p>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
+
+export default Introduction;
